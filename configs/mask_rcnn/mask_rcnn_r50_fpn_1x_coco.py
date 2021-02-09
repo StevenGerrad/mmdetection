0abbf8d29fecebb51bc9391c78d70bd1e383a419
StevenGerrad <1339807619@qq.com>
2021-02-09 12:46:36 +0000

    第一次提交

--- configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco.py
@@ -3,3 +3,7 @@ _base_ = [
     '../_base_/datasets/coco_instance.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+num_classes=6,#类别数+1
+img_scale=(1024,1024), #输入图像尺寸的最大边与最小边（train、val、test这三处都要修改）
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001) #当gpu数量为8时,lr=0.02；当gpu数量为4时,lr=0.01；我只要一个gpu，所以设置lr=0.0025
